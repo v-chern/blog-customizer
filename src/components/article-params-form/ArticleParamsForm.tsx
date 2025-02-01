19078cf570b098258ef269887fc8053ee5d55e0e
v-chern <vladimir.v.chernikov@yandex.ru>
2025-02-01 20:13:37 +0100

    blog customizer

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -2,16 +2,119 @@ import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
 
 import styles from './ArticleParamsForm.module.scss';
+import React from 'react';
+import { SyntheticEvent } from 'react';
+import { Select } from 'src/ui/select';
 
-export const ArticleParamsForm = () => {
+
+import { ArticleStateType, backgroundColors, contentWidthArr, defaultArticleState, fontColors, fontFamilyOptions, fontSizeOptions, OptionType } from 'src/constants/articleProps';
+import { Separator } from 'src/ui/separator';
+import { RadioGroup } from 'src/ui/radio-group';
+import { createPortal } from 'react-dom';
+
+type TArticleParamsFormProps = {
+	currentStyle: ArticleStateType;
+	onSubmit: (newStyle: ArticleStateType) => void;
+}
+
+export const ArticleParamsForm = ({currentStyle, onSubmit}: TArticleParamsFormProps) => {
+	const [formState, setFormState] = React.useState(false);
+	const [formValues, setFormValues] = React.useState<ArticleStateType>(currentStyle);
+
+	const containterRef = React.useRef<HTMLElement | null> (null);
+
+	const toggleForm = () => {
+		setFormState(!formState);
+		containterRef.current?.classList.toggle(styles.container_open)
+	}
+
+	const handleFormReset = () => {
+		setFormValues({...defaultArticleState});
+		onSubmit({...defaultArticleState});
+		toggleForm();
+	};
+
+	const handleFormSubmit = (e: SyntheticEvent) => {
+		e.preventDefault();
+		onSubmit(formValues);
+		toggleForm();
+	};
+
+	const handleExternalClick = (event: MouseEvent) => {
+		if (formState && containterRef.current && !containterRef.current.contains(event.target as Node) ) {
+			toggleForm();
+		}
+	}
+
+	React.useEffect(() => {
+		document.addEventListener('mousedown', handleExternalClick);
+		return () => {
+			document.removeEventListener('mousedown', handleExternalClick);
+		};
+	}, [formState]);
+	
 	return (
 		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
+			<ArrowButton 
+				isOpen={formState} 
+				onClick={toggleForm} />
+			<aside 
+				className={styles.container} 
+				ref={containterRef}>
 				<form className={styles.form}>
+					<div className={styles.inputsContainer}>
+						<h2 className={styles.title}>Задайте параметры</h2>
+						<Select 
+							title='Шрифт'
+							selected={formValues.fontFamilyOption} 
+							options={fontFamilyOptions}
+							onChange={(selected: OptionType) =>
+								setFormValues((prev) => ({ ...prev, fontFamilyOption: selected }))
+							}
+						/>
+						
+						<RadioGroup
+							title='Размер шрифта'
+							selected={formValues.fontSizeOption}
+							options={fontSizeOptions}
+							onChange={(selected: OptionType) =>
+								setFormValues((prev) => ({ ...prev, fontSizeOption: selected }))
+							}
+							name='font-size'
+						/>
+
+						<Select 
+							title='Цвет шрифта'
+							selected={formValues.fontColor} 
+							options={fontColors}
+							onChange={(selected: OptionType) =>
+								setFormValues((prev) => ({ ...prev, fontColor: selected }))
+							}
+						/>
+
+						<Separator />
+
+						<Select
+							title='Цвет фона'
+							selected={formValues.backgroundColor}
+							options={backgroundColors}
+							onChange={(selected: OptionType) =>
+								setFormValues((prev) => ({ ...prev, backgroundColor: selected }))
+							}
+						/>
+
+						<Select
+							title='Ширина контента'
+							selected={formValues.contentWidth}
+							options={contentWidthArr}
+							onChange={(selected: OptionType) =>
+								setFormValues((prev) => ({ ...prev, contentWidth: selected }))
+							}
+						/>
+					</div>
 					<div className={styles.bottomContainer}>
-						<Button title='Сбросить' htmlType='reset' type='clear' />
-						<Button title='Применить' htmlType='submit' type='apply' />
+						<Button title='Сбросить' htmlType='reset' type='clear' onClick={handleFormReset}/>
+						<Button title='Применить' htmlType='submit' type='apply' onClick={handleFormSubmit}/>
 					</div>
 				</form>
 			</aside>
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
 import { createRoot } from 'react-dom/client';
-import { StrictMode, CSSProperties } from 'react';
+import { StrictMode, CSSProperties, useState } from 'react';
 import clsx from 'clsx';
 
 import { Article } from './components/article/Article';
 import { ArticleParamsForm } from './components/article-params-form/ArticleParamsForm';
-import { defaultArticleState } from './constants/articleProps';
+import { ArticleStateType, defaultArticleState } from './constants/articleProps';
 
 import './styles/index.scss';
 import styles from './styles/index.module.scss';
@@ -13,19 +13,31 @@ const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
 
 const App = () => {
+
+	let currentStyle: ArticleStateType = {...defaultArticleState};
+
+	const getCSSProps = (style: ArticleStateType) => {
+		return {
+			'--font-family': style.fontFamilyOption.value,
+			'--font-size': style.fontSizeOption.value,
+			'--font-color': style.fontColor.value,
+			'--container-width': style.contentWidth.value,
+			'--bg-color': style.backgroundColor.value,
+		} as CSSProperties;
+	};
+
+	const [mainStyle, setMainStyle] = useState<CSSProperties>(getCSSProps(currentStyle));
+
+	const updateStyle = (newStyle: ArticleStateType) => {
+		currentStyle = newStyle;
+		setMainStyle(getCSSProps(currentStyle));
+	}
+
 	return (
 		<main
 			className={clsx(styles.main)}
-			style={
-				{
-					'--font-family': defaultArticleState.fontFamilyOption.value,
-					'--font-size': defaultArticleState.fontSizeOption.value,
-					'--font-color': defaultArticleState.fontColor.value,
-					'--container-width': defaultArticleState.contentWidth.value,
-					'--bg-color': defaultArticleState.backgroundColor.value,
-				} as CSSProperties
-			}>
-			<ArticleParamsForm />
+			style={mainStyle}>
+			<ArticleParamsForm currentStyle={currentStyle} onSubmit={updateStyle}/>
 			<Article />
 		</main>
 	);
